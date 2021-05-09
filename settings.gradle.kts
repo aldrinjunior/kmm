59c6926619982a3d05cde7a35770b2b8f8924bd3
aldrin junior <aldrinjr@aldrins-MacBook-Pro.local>
2021-05-09 16:15:36 -0300

    base-project-ios-android

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Application"
+include(":androidApp")
+include(":shared")
